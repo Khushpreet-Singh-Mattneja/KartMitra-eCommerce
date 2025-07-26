d4eccde2357c03bf6b19aae8358ce73a877bd8d3
https://github.com/Khushpreet-Singh-Mattneja <khushpreetsinghmattneja@gmail.com>
2025-07-26 23:09:07 +0530

    Banner Frontend Done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,14 +3,25 @@ import BannerImg from '../../assets/images/hero-img.png'
 
 const Banner = () => {
   return (
-    <div className='relative flex items-center bg-cover flex-start bg-center h-svh w-full' style={{backgroundImage
+    <div className='relative flex items-center bg-cover flex-start bg-center h-[65dvh] w-full' style={{backgroundImage
     : `url(${BannerImg})`}}>
-        <div className='absolute inset-0 bg-black opacity-50'></div>
-        <div className='relative z-10 text-white p-8'>
-            <h1 className='text-5xl font-bold mb-4'>Welcome to KartMitra</h1>
-            <p className='text-lg mb-6'>Discover the latest trends in fashion and shop your favorite styles.</p>
-            <button className='bg-white text-black px-6 py-3 rounded hover:bg-gray-200'>Shop Now</button>
+        <div className='w-1/2 flex ml-auto items-center justify-center'>
+            <div className='text-white text-center w-full'>
+                <h1 className='text-6xl font-extrabold mb-4 text-yellow-300 transition-all duration-700'>Welcome to KartMitra</h1>
+                <p className='text-2xl font-bold italic mb-6 '>Redefine your wardrobe with the latest fashion trends.</p>
+                <div className="flex justify-center gap-6 mt-8">
+                    <button className="bg-white text-black text-lg font-semibold px-8 py-3 rounded-md shadow hover:bg-black hover:text-white transition duration-300">
+                        New Trends
+                    </button>
+                    
+                    <button className="bg-white text-black text-lg font-semibold px-8 py-3 rounded-md shadow hover:bg-black hover:text-white transition duration-300">
+                        Shop Now
+                    </button>
+                </div>
+
+            </div>
         </div>
+
     </div>
   )
 }
